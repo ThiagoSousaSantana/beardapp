a1548c748d5b40b3a54286ca70b7bb686a7ab5a8
Thiago Santana <thiago91098@gmail.com>
2022-07-13 10:53:07 -0300

    first commit

---
+++ src/main/java/br/com/beardapp/BeardappApplication.java
@@ -0,0 +1,13 @@
+package br.com.beardapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeardappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeardappApplication.class, args);
+	}
+
+}
